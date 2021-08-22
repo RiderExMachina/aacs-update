e8c5a3edd8948eb5a3d5f2dda72c7295d2c9839d
riderexmachina <riderexmachina@protonmail.com>
2021-08-22 01:33:35 -0700

    Fix Updater.sh and add a log. Update install.sh to automatically start service

--- aacs-updater.sh
+++ aacs-updater.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+LOGFILE="/var/log/aacs-updater.log"
+
+
+echo "$(date -u) - Starting AACS update" >> $LOGFILE
 wget "http://fvonline-db.bplaced.net/fv_download.php?lang=eng" -O /tmp/aacs.zip
-unzip /tmp/aacs.zip
+unzip /tmp/aacs.zip -d /tmp
 
 for USER in $(ls /home); do
-	if -d [/home/$USER/.config/aacs]
-		cp /tmp/KEYDB.cfg /home/$USER/.config/aacs/
-		chown $USER:$USER /home/$USER/.config/aacs/*
+	if [ -d "/home/$USER/.config/aacs" ]; then
+		if [ $(getent passwd $USER >/dev/null) -eq 0)					
+			cp /tmp/keydb.cfg "/home/$USER/.config/aacs/KEYDB.cfg"
+			chown -R "$USER":"$USER" "/home/$USER/.config/aacs/"
+		fi
 	fi
 done
+
+echo "$(date -u) - Cleaning up" >> $LOGFILE
+rm /tmp/aacs.zip /tmp/keydb.cfg
--- install.sh
+++ install.sh
@@ -7,3 +7,10 @@ fi
 cp aacs-updater.sh /opt/
 cp aacs-update.service /etc/systemd/system/
 cp aacs-update.timer /etc/systemd/system/
+
+systemctl enable aacs-update.service
+systemctl start aacs-update.service
+
+if [[!$ -ne 0]]; then
+	echo "Something went wrong. Please create a pull request with the information from 'systemctl status aacs-update'"
+fi
